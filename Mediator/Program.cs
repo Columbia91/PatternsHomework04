bbc9385aa79efd59ccd88cfd7fb889467a99e0fc
globalmail91@gmail.com <globalmail91@gmail.com>
2019-07-26 17:42:40 +0300

    Добавьте файлы проекта.

---
+++ Mediator/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace Mediator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ManagerMediator mediator = new ManagerMediator();
+            Colleague factory = new Factory(mediator);
+            Colleague courier = new Courier(mediator);
+            Colleague shop = new Shop(mediator);
+
+            mediator.Factory = factory;
+            mediator.Courier = courier;
+            mediator.Shop = shop;
+
+            factory.Send("Есть продукция, необходимо доставить");
+            courier.Send("Продукция доставлена, необходимо сбыть");
+            shop.Send("Продукция успешно реализована");
+
+            Console.Read();
+        }
+    }
+
+    abstract class Mediator
+    {
+        public abstract void Send(string msg, Colleague colleague);
+    }
+    abstract class Colleague
+    {
+        protected Mediator mediator;
+
+        public Colleague(Mediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        public virtual void Send(string message)
+        {
+            mediator.Send(message, this);
+        }
+        public abstract void Notify(string message);
+    }
+    
+    class Factory : Colleague
+    {
+        public Factory(Mediator mediator)
+        : base(mediator)
+        { }
+
+        public override void Notify(string message)
+        {
+            Console.WriteLine("Сообщение заводу: " + message);
+        }
+    }
+    
+    class Courier : Colleague
+    {
+        public Courier(Mediator mediator)
+        : base(mediator)
+        { }
+
+        public override void Notify(string message)
+        {
+            Console.WriteLine("Сообщение курьеру: " + message);
+        }
+    }
+    
+    class Shop : Colleague
+    {
+        public Shop(Mediator mediator)
+        : base(mediator)
+        { }
+
+        public override void Notify(string message)
+        {
+            Console.WriteLine("Сообщение магазину: " + message);
+        }
+    }
+
+    class ManagerMediator : Mediator
+    {
+        public Colleague Factory { get; set; }
+        public Colleague Courier { get; set; }
+        public Colleague Shop { get; set; }
+        public override void Send(string msg, Colleague colleague)
+        {
+            // если отправитель - завод, значит есть продукт
+            // отправляем сообщение курьеру - доставить продукцию
+            if (Factory == colleague)
+                Courier.Notify(msg);
+            // если отправитель - курьер, значит товар доставлен
+            // отправляем сообщение в магазин
+            else if (Courier == colleague)
+                Shop.Notify(msg);
+            // если отправитель - магазин, значит продукция сбыта
+            // отправляем сообщение заводу
+            else if (Shop == colleague)
+                Factory.Notify(msg);
+        }
+    }
+}
---
+++ Memento/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Memento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // человек в бассейне
+            Man man = new Man();
+            // он идет в раздевалку
+            DressingRoom room = new DressingRoom();
+
+            man.Clothes = "Футболка, шорты, кроссовки";
+
+            // кладет свои вещи в сумку
+            // и оставляет на хранение в раздевалку
+            room.Bag = man.Undress();
+
+            // одевает подходящие одежду и снаряжение
+            man.Clothes = "Плавки, подводные очки";
+
+            // после обратно переодевается
+            man.Dress(room.Bag);
+            room.Bag = man.Undress();
+        }
+    }
+
+    class Man
+    {
+        public string Clothes { get; set; }
+        public void Dress(Bag bag)
+        {
+            Clothes = bag.Contents;
+        }
+        public Bag Undress()
+        {
+            return new Bag(Clothes);
+        }
+    }
+
+    class Bag
+    {
+        public string Contents { get; set; }
+        public Bag(string contents)
+        {
+            this.Contents = contents;
+        }
+    }
+    class DressingRoom
+    {
+        public Bag Bag { get; set; }
+    }
+}
